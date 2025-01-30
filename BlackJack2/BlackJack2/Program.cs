0955baa8a8db08907e36050d90472c5cab239931
Argenika <angelalepeshko560@gmail.com>
2025-01-30 20:20:43 +0100

    update

--- BlackJack2/BlackJack2/Program.cs
+++ BlackJack2/BlackJack2/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Xml.Serialization;
+
+class Program {
+    static Random random = new Random(); 
+    static List<string> baraja = new List<string>();
+    static Dictionary<string,int>cartasValor = new Dictionary<string, int>();
+
+static void Main ()
+{
+   InicioBaraja();
+   
+    List<string> manoJugador = new List<string>();
+    List<string> manoBanca = new List<string>();
+    int puntuacionJugador = 0, puntuacionBanca = 0;
+
+    Console.WriteLine("Bienvenido al Blackjack!");
+
+
+while(true){
+  string carta = CogerCarta();
+  manoJugador.Add(carta);
+}
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+}
